d88e776307f84fd0d45a6d05f008da0bcf179213
Jaksa Malisic <jaksa.malisic@gmail.com>
2019-02-19 20:22:05 +0100

    build

---
+++ build/precache-manifest.eba5f3af9d71623d8c072df79d9fc3b0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1dbbe8a370c893df41e7",
+    "url": "/static/js/main.1dbbe8a3.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "c2541c7cf249335b8360",
+    "url": "/static/css/2.fb97d6c6.chunk.css"
+  },
+  {
+    "revision": "c2541c7cf249335b8360",
+    "url": "/static/js/2.c2541c7c.chunk.js"
+  },
+  {
+    "revision": "105b749c1a93551c296a03e30598510d",
+    "url": "/static/media/world.105b749c.svg"
+  },
+  {
+    "revision": "867f7f0e1c5e24fe1d59efc2f9a89dcf",
+    "url": "/static/media/search.867f7f0e.svg"
+  },
+  {
+    "revision": "e0fd54df279fe02fb261a1e379ad36fa",
+    "url": "/static/media/toggle.e0fd54df.svg"
+  },
+  {
+    "revision": "95cb07ec9aab2d512d9f354f4c0f9cc9",
+    "url": "/index.html"
+  }
+];
