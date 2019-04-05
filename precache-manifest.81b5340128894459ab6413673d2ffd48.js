a58861d6db5157f0326285b329fb9c1da102d7e8
Jaksa Malisic <jaksa.malisic@gmail.com>
2019-04-05 18:22:28 +0200

    Updates

---
+++ precache-manifest.81b5340128894459ab6413673d2ffd48.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "64ed23799dc9c6330001",
+    "url": "/tijana-adamovic/static/js/main.64ed2379.chunk.js"
+  },
+  {
+    "revision": "8e43a3218d10ce406bbf",
+    "url": "/tijana-adamovic/static/js/runtime~main.8e43a321.js"
+  },
+  {
+    "revision": "b0cc782fc2684534145f",
+    "url": "/tijana-adamovic/static/css/2.fb97d6c6.chunk.css"
+  },
+  {
+    "revision": "b0cc782fc2684534145f",
+    "url": "/tijana-adamovic/static/js/2.b0cc782f.chunk.js"
+  },
+  {
+    "revision": "e0fd54df279fe02fb261a1e379ad36fa",
+    "url": "/tijana-adamovic/static/media/toggle.e0fd54df.svg"
+  },
+  {
+    "revision": "9d467ce74629f0e4e9a9eb64d5739de7",
+    "url": "/tijana-adamovic/static/media/more.9d467ce7.svg"
+  },
+  {
+    "revision": "5515f1b6f063763dbf7de2409b3bb2de",
+    "url": "/tijana-adamovic/static/media/canada.5515f1b6.jpg"
+  },
+  {
+    "revision": "7886ee254d69cfa5f869ab85ed0eb3de",
+    "url": "/tijana-adamovic/static/media/facebook.7886ee25.svg"
+  },
+  {
+    "revision": "46570a3ae9fcfcf5370883f66093a9c2",
+    "url": "/tijana-adamovic/static/media/instagram.46570a3a.svg"
+  },
+  {
+    "revision": "4f6e5146be6124dca4af9f0750cfcf12",
+    "url": "/tijana-adamovic/static/media/uk.4f6e5146.jpg"
+  },
+  {
+    "revision": "2534c1caa2502203e1bd4e1fe3473120",
+    "url": "/tijana-adamovic/static/media/usa.2534c1ca.jpg"
+  },
+  {
+    "revision": "0e4d485ab43a04813cdba3aa95c5aecb",
+    "url": "/tijana-adamovic/static/media/esta.0e4d485a.jpg"
+  },
+  {
+    "revision": "51bc8b69255bc65486b02e27935dfcf1",
+    "url": "/tijana-adamovic/static/media/passport.51bc8b69.jpeg"
+  },
+  {
+    "revision": "67d91d4c62959e810895e9af757c5624",
+    "url": "/tijana-adamovic/static/media/world.67d91d4c.svg"
+  },
+  {
+    "revision": "e974111f2f8a6be98bee3300c055732c",
+    "url": "/tijana-adamovic/index.html"
+  }
+];
